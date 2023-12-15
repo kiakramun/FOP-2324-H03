4f12461fae468d7056e55568afdb342523c581c0
Krazmer <kiakramun@gmail.com>
2023-12-15 04:53:06 +0100

    first commit

---
+++ FOP-2324-H03-Student-main/src/main/java/h03/Main.java
@@ -0,0 +1,17 @@
+package h03;
+
+import fopbot.World;
+
+/**
+ * Main entry point in executing the program.
+ */
+public class Main {
+    /**
+     * Main entry point in executing the program.
+     *
+     * @param args program arguments, currently ignored
+     */
+    public static void main(String[] args) {
+        World.setVisible(true);
+    }
+}
