824174f7f4a73a0f3337d50ad551aa739753d8a0
Krazmer <kiakramun@gmail.com>
2023-12-15 06:20:59 +0100

    add RGBRobots.java

---
+++ FOP-2324-H03-Student-main/src/main/java/h03/robots/RGBRobots.java
@@ -0,0 +1,39 @@
+package h03.robots;
+
+import fopbot.RobotFamily;
+import static fopbot.RobotFamily.SQUARE_RED;
+import static fopbot.RobotFamily.SQUARE_GREEN;
+import static fopbot.RobotFamily.SQUARE_BLUE;
+
+public class RGBRobots extends MultiFamilyRobot {
+
+    /**
+     * A sequence of a red, green and blue square robot.
+     */
+    private static final RobotFamily[] RGB = new RobotFamily[]{SQUARE_RED,SQUARE_GREEN,SQUARE_BLUE};
+
+    /**
+     * A sequence of a blue, red and green square robot.
+     */
+    private static final RobotFamily[] BRG = new RobotFamily[]{SQUARE_BLUE,SQUARE_RED,SQUARE_GREEN};
+
+    /**
+     * Constructs a {@link RGBRobots} on the given position with a sequence of the robot families
+     * {@link RobotFamily#SQUARE_RED}, {@link RobotFamily#SQUARE_BLUE}, {@link RobotFamily#SQUARE_GREEN}
+     * @param x the position on the x-axis
+     * @param y the posotion on the y-axis
+     * @param inverted whether the sequence of predefined robot families should be inverted
+     */
+    public RGBRobots(final int x,final int y,final boolean inverted){
+        super(x,y, !inverted? RGB : BRG);
+    }
+
+    /**
+     *  Exchanges the current robot 3 times to cycle through all robots
+     */
+    public void testRGB(){
+        for (int i = 0; i < 3; i++) {
+            exchange();
+        }
+    }
+}
